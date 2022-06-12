8694cf5d1e52f03abf713b94b0bd7990b3753192
Aditya MP <211wadityaw112@gmail.com>
2022-06-12 17:45:41 +0700

    change main class to Main.java

--- src/pacman/Main.java
+++ src/pacman/Main.java
@@ -1,22 +1,16 @@
 package pacman;
 
 import pacman.controllers.PacmanController;
-import pacman.models.Level;
-import pacman.views.ScreenSettings;
+import pacman.views.MainView;
+
 
 public class Main {
     public static void main(String[] args){
         //PacmanController mainController = new PacmanController();
-        Level test1 = new Level();
-        test1.save();
-        ScreenSettings test2 = new ScreenSettings();
-        ScreenSettings.save();
-        System.out.println(test1);
-        System.out.println(test2);
-        
-        for(int b : Level.levels[0]){
-            System.out.println((short) b);
-        }
+        PacmanController controller = new PacmanController();
+        MainView view = new MainView(controller);
+        view.setTitle("JPacman");
+        view.setVisible(true);
         
     }
 }
