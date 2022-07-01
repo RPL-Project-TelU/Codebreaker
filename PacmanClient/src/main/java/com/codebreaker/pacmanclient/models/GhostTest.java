aac191e6a9b7a682b67b1d4f7cd6bc0fb2d461d3
FarH.K <farhanhisyamk@protonmail.com>
2022-07-02 01:03:25 +0700

    setup pacman dan test unit

---
+++ PacmanClient/src/main/java/com/codebreaker/pacmanclient/models/GhostTest.java
@@ -0,0 +1,32 @@
+package pacman.models;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GhostTest {
+    @Test
+    public void testEquals1() {
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEquals2() {
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEquals3() {
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testImage() {
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(expected, actual);
+    }
+}
