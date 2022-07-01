2b1fe927d830a56abca7c065089bd784fd01bed4
Aditya MP <211wadityaw112@gmail.com>
2022-07-01 22:21:49 +0700

    Revert "change dir of test class"
    This reverts commit d1ee8e924f0e08ba0a3335bcfdcb9678e7d04c3d.

---
+++ Codebreaker/src/pacman/Main.java
@@ -0,0 +1,18 @@
+package pacman;
+
+
+
+import pacman.controllers.PacmanController;
+import pacman.views.MainView;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // PacmanController mainController = new PacmanController();
+        PacmanController controller = new PacmanController();
+        MainView view = new MainView(controller);
+        view.setTitle("JPacman");
+        view.setVisible(true);
+
+    }
+}
