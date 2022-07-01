29662e9c2998b835f495f6a76cf3ce2743181e79
habibmirsyad <habmirs@gmail.com>
2022-07-02 01:16:10 +0700

    testContinueLevel

--- PacmanClient/src/main/java/com/codebreaker/pacmanclient/services/PacmanServiceTest.java
+++ PacmanClient/src/main/java/com/codebreaker/pacmanclient/services/PacmanServiceTest.java
@@ -12,7 +12,23 @@ public class PacmanServiceTest {
 
     @Test
     public void testContinueLevel() {
+        final int validSpeeds[] = { 1, 2, 3, 4, 6, 8 };
+        
+        Ghost ghost = new Ghost();
+        ghost.setSpeed(validSpeeds[1]);  //Speed awal = 2
+        ps.ghosts[0] = ghost;
 
+        int sebelum = ps.ghosts[0].getSpeed(); // == 2
+        ps.continueLevel();
+        int sesudah = ps.ghosts[0].getSpeed(); // >= 2
+        
+        for (int i = 0; i < validSpeeds.length; i++){
+            if (sebelum > sesudah){
+                assertEquals(validSpeeds[i], sesudah);
+            }else if (sebelum <= sesudah && validSpeeds[i] == sesudah){
+                assertEquals(validSpeeds[i], sesudah);
+            }
+        }
     }
 
     @Test
